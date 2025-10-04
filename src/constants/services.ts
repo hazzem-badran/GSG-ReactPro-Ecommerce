527d7c88b14718fca5374d606f56d30f277565d4
Hazem Badran <hazemaqbadran@gmail.com>
2025-10-04 17:34:42 +0300

    feat/ECOM-012-services

---
+++ src/constants/services.ts
@@ -0,0 +1,17 @@
+export const SERVICES = [
+  {
+    //   icon: Truck,
+    title: "FREE AND FAST DELIVERY",
+    description: "Free delivery for all orders over $140",
+  },
+  {
+    //   icon: Headphones,
+    title: "24/7 CUSTOMER SERVICE",
+    description: "Friendly 24/7 customer support",
+  },
+  {
+    //   icon: ShieldCheck,
+    title: "MONEY BACK GUARANTEE",
+    description: "We return money within 30 days",
+  },
+];
