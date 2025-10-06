b73e50aaebc24229afa5d24017405e0356e19ab0
Hazem Badran <hazemaqbadran@gmail.com>
2025-10-06 08:49:36 +0300

    fix/ECOM-017

--- src/components/ui/navigation/header/TopBanner.tsx
+++ src/components/ui/navigation/header/TopBanner.tsx
@@ -6,21 +6,26 @@ const TopBanner = () => {
       <Container>
         <div className="flex items-center justify-between text-sm">
           <div className="flex-1" />
-          
+
           <div className="text-center">
-            <span>Summer Sale For All Swim Suits And Free Express Delivery - OFF 50%!</span>
-            <a 
-              href="#" 
+            <span>
+              <span className="hidden sm:inline">
+                Summer Sale For All Swim Suits And Free
+              </span>{" "}
+              Express Delivery - OFF 50%!
+            </span>
+
+            <a
+              href="#"
               className="font-semibold underline ml-2 hover:text-gray-300 transition-colors"
             >
               ShopNow
             </a>
           </div>
-          
+
           <div className="flex-1 flex justify-end">
             <select className="bg-transparent text-white border-none text-sm cursor-pointer focus:outline-none">
               <option className="text-black">English</option>
-              <option className="text-black">العربية</option>
             </select>
           </div>
         </div>
